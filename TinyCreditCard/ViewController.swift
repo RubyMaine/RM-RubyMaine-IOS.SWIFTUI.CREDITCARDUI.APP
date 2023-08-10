c7ae88bf467204a7cf432ed0cfde728b3e40637c
RubyMaine <104354155+RubyMaine@users.noreply.github.com>
2023-08-11 00:19:52 +0500

    𒆜 𝗥𝘂𝗯𝘆𝗠𝗮𝗶𝗻𝗲 𒆜 — 𝗜𝗢𝗦 𝗦𝗪𝗜𝗙𝗧𝗨𝗜 𝗖𝗥𝗘𝗗𝗜𝗧𝗖𝗔𝗥𝗗.𝗔𝗣𝗣 [𝗜𝗢𝗦*𝟭𝟲.𝟰, 𝗫𝗰𝗼𝗱𝗲*𝟭𝟰.𝟯.𝟭, 𝗦𝗪𝗜𝗙𝗧*𝟱.𝟴.𝟭 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱] — [𝟭𝟭.𝟬𝟴.𝟮𝟬𝟮𝟯] для личного портфолио 𒆜 𝗥𝘂𝗯𝘆𝗠𝗮𝗶𝗻𝗲 𒆜
    𒆜 𝗥𝘂𝗯𝘆𝗠𝗮𝗶𝗻𝗲 𒆜 — 𝗜𝗢𝗦 𝗦𝗪𝗜𝗙𝗧𝗨𝗜 𝗖𝗥𝗘𝗗𝗜𝗧𝗖𝗔𝗥𝗗.𝗔𝗣𝗣 [𝗜𝗢𝗦*𝟭𝟲.𝟰, 𝗫𝗰𝗼𝗱𝗲*𝟭𝟰.𝟯.𝟭, 𝗦𝗪𝗜𝗙𝗧*𝟱.𝟴.𝟭 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱] — [𝟭𝟭.𝟬𝟴.𝟮𝟬𝟮𝟯] для личного портфолио 𒆜 𝗥𝘂𝗯𝘆𝗠𝗮𝗶𝗻𝗲 𒆜

---
+++ TinyCreditCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinyCreditCard
+//
+//  Created by RubyMaine on 10/08/2023.
+//  Copyright © 2019 @GRubyMaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
